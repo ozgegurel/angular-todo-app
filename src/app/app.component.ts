43533b8f741290d9ab160a956697ce91fa947533
ozgegurel <ozgegurel94@gmail.com>
2020-08-14 23:21:06 +0300

    add new component input-button-unit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
+
+  <app-input-button-unit></app-input-button-unit>
+`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'my-todo-list';
 }
