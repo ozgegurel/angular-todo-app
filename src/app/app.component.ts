0ed34f60ce729a602830d2fe4a746b13b91c5210
ozgegurel <ozgegurel94@gmail.com>
2020-08-15 01:40:54 +0300

    add ng directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,32 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `
+
   <h1>
     Welcome to {{ title }}!
   </h1>
+  
+  <h1 *ngIf="userLoggedIn">Welcome!</h1>
 
   <app-input-button-unit></app-input-button-unit>
+
+  <ul>
+    <li *ngFor="let todoItem of todoList">
+      {{ todoItem.title }}
+    </li>
+  </ul>
 `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-todo-list';
+  userLoggedIn = true;
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
