4bb792c57f65762e863c783460b747e970ed66e4
ozgegurel <ozgegurel94@gmail.com>
2020-08-16 16:59:51 +0300

    add feature of remove item

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -10,7 +10,8 @@ import { TodoListService } from '../services/todo-list.service';
 
       <ul>
         <li *ngFor="let todoItem of todoList">
-          <app-todo-item [item]="todoItem"></app-todo-item>
+          <app-todo-item [item]="todoItem"
+                         (remove)="removeItem($event)"></app-todo-item>
         </li>
       </ul>
     </div>
@@ -30,4 +31,9 @@ export class ListManagerComponent implements OnInit {
   addItem(title: string) {
     this.todoListService.addItem({ title });
   }
+
+  removeItem(item) {
+  this.todoListService.deleteItem(item);
+  }
+
 }
--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
-  template: ' <div class="todo-item"> {{ item.title }} </div>',
+  template: `
+    <div class="todo-item">
+      {{ item.title }}
+
+      <button class="gg-trash" (click)="removeItem()"> 
+      </button>
+    </div>
+  `,
   styleUrls: ['./todo-item.component.css']
 })
+
 
 
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem;
 
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  removeItem() {
+    this.remove.emit(this.item);
   }
 
 }
